95e55a40a73fea6aecf83a25e1e6cce31b286dc7
Grace <grace.anderson.au@gmail.com>
2022-05-25 21:24:44 +1000

    Added blogPost model, routes, html page

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class BlogPost extends Model {}
 
-Project.init(
+BlogPost.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -40,8 +40,8 @@ Project.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'project',
+    modelName: 'blogPost',
   }
 );
 
-module.exports = Project;
+module.exports = BlogPost;
