857ddde9ddd2e6ea23882dc1a519cca2c6563f5e
Grace <grace.anderson.au@gmail.com>
2022-05-29 09:44:31 +1000

    Updated date format

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,19 @@
 module.exports = {
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
+    // Format date as DD/MM/YYYY
+
+    let current_datetime = new Date();
+    let formatted_date =
+      current_datetime.getDate() +
+      '-' +
+      (current_datetime.getMonth() + 1) +
+      '-' +
+      current_datetime.getFullYear();
+    console.log(formatted_date);
+
+    // return date.toLocaleDateString();
+
+    return formatted_date;
   },
+
 };
