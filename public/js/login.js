0afd1d9562f2d1135e9ba0318d0c99895c19ce8e
Grace <grace.anderson.au@gmail.com>
2022-05-30 07:49:00 +1000

    reseeded db

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,25 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-    // Make sure the username and password are filled in
-    if (username && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+  // username and password required
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('Failed to log in.');
-        }
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in.');
     }
+  }
 };
 
 document
-    .querySelector('#login-form')
-    .addEventListener('submit', loginFormHandler);
+  .querySelector('#login-form')
+  .addEventListener('submit', loginFormHandler);
--- seeds/index.js
+++ seeds/index.js
@@ -4,18 +4,18 @@ const seedPosts = require('./postData');
 const seedComments = require('./commentData');
 
 const seedAll = async () => {
-    await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
 
-    // Load user data for testing
-    await seedUsers();
+  // Load user data for testing
+  await seedUsers();
 
-    // Load blog post data for testing
-    await seedPosts();
+  // Load blog post data for testing
+  await seedPosts();
 
-    // Load blog past data for testing
-    await seedComments();
+  // Load blog past data for testing
+  await seedComments();
 
-    process.exit(0);
+  process.exit(0);
 };
 
 seedAll();
