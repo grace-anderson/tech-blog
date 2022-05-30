bf9fa652f53022a047a9347e5f0dc20f128480a5
Grace <grace.anderson.au@gmail.com>
2022-05-30 10:24:17 +1000

    fixed up new post

--- public/js/blogUpdate.js
+++ public/js/blogUpdate.js
@@ -1,47 +1,47 @@
 const updatePostFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Get the blog post ID from the end of the URL
-    const postId = window.location.href.split("/").pop();
+  // Get the blog post ID from the end of the URL
+  const postId = window.location.href.split('/').pop();
 
-    const title = document.querySelector('#title-input').value;
-    const contents = document.querySelector('#content-input').value;
+  const title = document.querySelector('#title-input').value;
+  const contents = document.querySelector('#content-input').value;
 
-    const response = await fetch('/api/blog/' + postId, {
-        method: 'PUT',
-        body: JSON.stringify({ title, contents }),
-        headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch('/api/blog/' + postId, {
+    method: 'PUT',
+    body: JSON.stringify({ title, contents }),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert('Failed to update blog post');
-    }
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to update blog post');
+  }
 };
 
 document
-    .querySelector('#blog-update-form')
-    .addEventListener('submit', updatePostFormHandler);
+  .querySelector('#blog-update-form')
+  .addEventListener('submit', updatePostFormHandler);
 
 const deletePostHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Get the blog post ID from the end of the URL
-    const postId = window.location.href.split("/").pop();
+  // Get the blog post ID from the end of the URL
+  const postId = window.location.href.split('/').pop();
 
-    const response = await fetch('/api/blog/' + postId, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch('/api/blog/' + postId, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert('Failed to delete blog post');
-    }
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Unable to delete post');
+  }
 };
 
 document
-    .querySelector('#delete-button')
-    .addEventListener('click', deletePostHandler);
+  .querySelector('#delete-button')
+  .addEventListener('click', deletePostHandler);
